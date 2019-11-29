1950249d4a32e72e026e141ddb1606776a74f8e8
Snahi <good.letterbox@gmail.com>
2019-11-29 16:22:22 +0100

    advanced search popup gui

---
+++ scripts/advanced_search.js
@@ -0,0 +1,10 @@
+// CONST
+const ADVANCED_SEARCH_POPUP_ID = "advanced_search_popup";
+
+
+
+function showAdvancedSearchPopup()
+{
+    let popup = document.getElementById(ADVANCED_SEARCH_POPUP_ID);
+    popup.style.display = "block";
+}
