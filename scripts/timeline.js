df940074246a7223979bd9caf2108198bc243d7f
BidGem <pietro.peppo@gmail.com>
2019-12-01 19:25:24 +0100

    timeline mockups

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -2,21 +2,56 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [ 'resourceTimeline' ],
+        schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+        plugins: [ 'resourceTimeline', 'interaction' ],
         defaultView: 'resourceTimelineWeek',
+        editable: true,
+        eventStartEditable: true,
+        eventResourceEditable: true,
         resources: [
             {
-                id: 'ballons',
-                title: 'Baloons'
+                id: 'balloons',
+                title: 'Balloons',
+                eventBackgroundColor: '#ff0000'
             },
             {
                 id: 'music',
-                title: 'Music'
+                title: 'Music',
+                eventBackgroundColor: '#18be00'
             }
         ],
         slotDuration: {day: 1},
-        resourceLabelText: 'Categories'
+        resourceLabelText: 'Categories',
+        events: [
+            {
+                id: 1,
+                title: 'Inflate the balloons',
+                start: getDay(0),
+                end: getDay(1),
+                resourceId: 'balloons'
+            },
+            {
+                id: 2,
+                title: 'New song mixing',
+                start: getDay(0),
+                end: getDay(2),
+                resourceId: 'music'
+            }
+        ]
     });
-
     calendar.render();
+    console.log(calendar.getEvents());
+
 });
+
+function getDay(daysSinceToday) {
+    var day = new Date();
+    day.setDate(day.getDate() + daysSinceToday);
+    var dd = String(day.getDay() + 1).padStart(2, '0');
+    var mm = String(day.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = day.getFullYear();
+
+    day = yyyy + '-' + mm + '-' + dd;
+    console.log(day);
+    return day
+}
